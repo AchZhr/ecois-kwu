43e1f0ed7e3aa6139cc19c2eb91683df68487c1e
Zharvi Achmadha <achzhr999@gmail.com>
2022-06-16 15:08:37 +0700

    home edit

--- application/views/home.php
+++ application/views/home.php
@@ -96,7 +96,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="section__text-wrap">
-                            <h1 class="section__heading u-c-secondary u-s-m-b-12">SHOP BY DEALS</h1>
+                            <h1 class="section__heading u-c-secondary u-s-m-b-12">Kategori</h1>
 
                             <span class="section__span u-c-silver">BROWSE FAVOURITE DEALS</span>
                         </div>
